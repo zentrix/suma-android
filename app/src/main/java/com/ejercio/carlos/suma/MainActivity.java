76080e1228c163c2fa27f23616cda17d9b746fbf
Carlos Villalvazo <zentrix.24@gmail.com>
2016-08-29 19:38:46 -0500

    Modificacion de la vista el para aceptar radiobutton

--- app/src/main/java/com/ejercio/carlos/suma/MainActivity.java
+++ app/src/main/java/com/ejercio/carlos/suma/MainActivity.java
@@ -1,9 +1,10 @@
 package com.ejercio.carlos.suma;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,17 +15,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //Funcion de sumar dos numeros
-    public void sumar(View view){
-
+    public void radiobtn(View view) {
         // Optenemos los valores
-        int v1 = Integer.parseInt(((EditText) findViewById(R.id.txtv1)).getText().toString());
-        int v2 = Integer.parseInt(((EditText) findViewById(R.id.txtv2)).getText().toString());
+        int su = Integer.parseInt(((EditText) findViewById(R.id.txtv1)).getText().toString());
+        int re = Integer.parseInt(((EditText) findViewById(R.id.txtv2)).getText().toString());
+
+        RadioButton rbtnsuma = (RadioButton) findViewById(R.id.rbtnS);
+        RadioButton rbtnresta = (RadioButton) findViewById(R.id.rbtnR);
 
         //Accedemos a la label para mostrar el resultado
         TextView res = (TextView) findViewById(R.id.lblResultado);
 
-        // Realizamos la suma y obtenemos mostramos el resultado en pantalla
-        res.setText(String.valueOf(v1 + v2));
+        if (rbtnsuma.isChecked())
+            res.setText(String.valueOf(su + re));
+        else if (rbtnresta.isChecked())
+            res.setText(String.valueOf(su - re));
     }
 }
